372b1563867506bd95a20edfd5db1122ca9694b6
rundon <1058950205@qq.com>
2022-11-14 16:12:04 +0800

    feat:已知bug

--- provider/src/main/java/com/onefly/united/view/utils/PdfUtils.java
+++ provider/src/main/java/com/onefly/united/view/utils/PdfUtils.java
@@ -21,7 +21,6 @@ import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
 
 @Slf4j
 @Component
